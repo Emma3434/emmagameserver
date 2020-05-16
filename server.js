5e609f002aabb1cd0af9aa26f5ac33e981d50f1c
Emma3434 <51844300+Emma3434@users.noreply.github.com>
2020-05-16 05:25:38 -0600

    Update server.js

--- server.js
+++ server.js
@@ -102,7 +102,7 @@ router.post('/signin', function(req, res) {
 // random test
 router.route('/random')
     .get(authJwtController.isAuthenticated, function (req, res){
-        res.status(200).json(success: true, message: "You are logged in!");
+        res.status(200).json({success: true, message: "You are logged in!"})
     });
 // discussion (single) routes
 router.route('/discussions/:discussionId')
