ab24939d20065dbfb31aa792ea836638bb26a460
Emma3434 <51844300+Emma3434@users.noreply.github.com>
2020-05-16 04:51:10 -0600

    dupe

--- server.js
+++ server.js
@@ -64,10 +64,10 @@ router.post('/signup', function(req, res) {
         user.save(function(err) {
             if (err) {
                 // duplicate entry
-                /*
+
                 if (err.code === 11000)
                     return res.json({ success: false, message: 'A user with that username already exists. '});
-                else*/
+                else
                     return res.send(err);
             }
 
