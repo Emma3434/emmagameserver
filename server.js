f55f6adda6186c447ebb984d7b6eaa80006a522e
Emma3434 <51844300+Emma3434@users.noreply.github.com>
2020-05-17 02:09:16 -0600

    error code

--- server.js
+++ server.js
@@ -65,7 +65,7 @@ router.post('/signup', function(req, res) {
             if (err) {
                 // duplicate entry
 
-                if (err.code === 11000)
+                if (err.code == 11000)
                     return res.json({ success: false, message: 'A user with that username already exists. '});
                 else
                     return res.send(err);
