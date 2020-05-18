5f38fa385f4b5227e68e392f9e765419264ee225
Emma3434 <51844300+Emma3434@users.noreply.github.com>
2020-05-17 21:01:41 -0600

    return in signin

--- server.js
+++ server.js
@@ -93,10 +93,10 @@ router.post('/signin', function(req, res) {
                 if (isMatch) {
                     var userToken = {id: user._id, username: user.username};
                     var token = jwt.sign(userToken, process.env.SECRET_KEY);
-                    res.status(200).json({success: true, username: user.username, token: 'JWT ' + token});
+                    return res.status(200).json({success: true, username: user.username, token: 'JWT ' + token});
                 }
                 else {
-                    res.status(401).send({success: false, message: 'Authentication failed.'});
+                    return res.status(401).send({success: false, message: 'Authentication failed.'});
                 }
             });
         }
